5c495fcac20fa28d9a601eb48e739f9294425be8
NoMeCompila <cabafer1@gmail.com>
2024-01-06 16:49:16 -0300

    Documentando texto con github Copilot

--- AllPrograms2.py
+++ AllPrograms2.py
@@ -3,7 +3,28 @@ from datetime import datetime
 from tabulate import tabulate
 
 
+def get_current_time() -> str:
+    """
+    Retorna la hora actual en formato hh:mm:ss
+    :return: str
+    """
+    return datetime.now().strftime("%H:%M:%S")
+
+
+def get_current_date() -> str:
+    """
+    Retorna la fecha actual en formato dd/mm/aaaa
+    :return: str
+    """
+    return datetime.now().strftime("%d/%m/%Y")
+
+
 def get_all_installed_programs(directory: str) -> list:
+    """
+    Retorna una lista de diccionarios con la informacion de los programas instalados
+    :param directory:
+    :return: list
+    """
     programs = []
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -24,6 +45,13 @@ def get_all_installed_programs(directory: str) -> list:
 
 
 def all_installed_programs(directory1: str, directory2: str) -> list:
+    """
+    Recibe como parametro dos directorios y retorna una lista de diccionarios con la informacion de los programas
+    en los directorios de program files y el de program files (x86) para obtener una sola lista de programas
+    :param directory1:
+    :param directory2:
+    :return: list
+    """
     return get_all_installed_programs(directory1) + get_all_installed_programs(directory2)
 
 
@@ -33,3 +61,4 @@ if __name__ == "__main__":
         program_list = [[program['name'], program['path'], program['size'], program['created at']]]
         print(tabulate(program_list, headers=head, tablefmt='fancy_grid', stralign='center', floatfmt='.0f'))
         print()
+
